4b04d57f7fdb4257b37d8b63fe9a5eed19b01d28
Kelly Hancox <hancoxk@mail.gvsu.edu>
2020-01-22 17:37:59 -0500

    added new files for tree structure

--- src/frontpage.js
+++ src/frontpage.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import logo from './shared/icons/logo.svg';
+import './frontpage.css';
 
 function App() {
   return (
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/frontpage.js</code> and save to reload.
         </p>
         <a
           className="App-link"
