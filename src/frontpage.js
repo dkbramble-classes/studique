7dbb406c2c8f975fc0c3e785e3ac464b2a9b689a
Dane <dkbrambled@gmail.com>
2020-02-01 18:29:44 -0500

    Fixed page size issue, cleaned up / removed unnecessary code

--- src/frontpage.js
+++ src/frontpage.js
@@ -1,62 +1,58 @@
 import React from 'react';
+import logo from "./shared/img/logo.png";
 //import logo from './shared/icons/logo.svg';
 //import Qlist from './question-list/Qlist';
 
 function App() {
   return (
-    <div className="App">
-
-    <head>
+    <div className="App bg-image">
         <link href="./bootstrap.min.css" rel="stylesheet"></link>
         <link href="./font-awesome.min.css" rel="stylesheet"></link>
-        <link href="./grayscale.min.css" rel="stylesheet"></link>
         <link href="https://fonts.googleapis.com/css?family=Varela+Round&display=swap" rel="stylesheet"></link>
-    </head>
-    <body>
-      <nav className="navbar navbar-expand-sm navbar-light fixed-top" id="mainNav">
+
+      <nav className="navbar navbar-expand-sm navbar-light" id="mainNav">
         <div className="container">
-          <a className="navbar-brand js-scroll-trigger" href="#page-top">Studique</a>
-          {/* <button className="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
-            HELLO
-            <i className="fa fa-bars"></i>
-          </button> */}
+          <a className="navbar-brand  text-font" href="#page-top">
+          <img className="brand-logo mr-2" src={logo} alt="LogoBrand"/>
+             Studique</a>
           <div className="collapse navbar-collapse" id="navbarResponsive">
             <ul className="navbar-nav ml-auto">
               <li className="nav-item">
-                <a className="nav-link js-scroll-trigger" href="#about">Sign in</a>
+              {/* <button type="button" class="btn btn-outline-info border border-info">Info</button> */}
+              <button type="submit" className=" btn nav-link text-font">Sign In</button>
               </li>
               <li className="nav-item">
-                <a className="nav-link js-scroll-trigger" href="#projects">Sign up</a>
+                <button className="nav-link btn text-font">Sign up</button>
               </li>
             </ul>
           </div>
         </div>
       </nav>
-    
-      <section id="signup" className="signup-section">
-        <div className="container">
+      
+      {/* <div className="jumbotron mt-10"></div> */}
+      <section id="signup" className="signup-section my-5 imgbox center-fit">
+        {/* <div className="jumbotron jumbotron-fluid rounded mt-10">
+        <div className="container-fluid"> */}
           <div className="row">
             <div className="col-md-5 col-lg-8 mx-auto text-center">
-              {/*<i className="far fa-paper-plane fa-2x mb-2 text-white"></i>*/}
               <h1 className="text-white mb-4">Studique</h1>
               <h3 className="text-white mb-5">Crowdsourcing The College Experience</h3>
-              
+                {/* <div className="media">
+                <img className="logo mr-3" src={logo} alt="LogoPage"/>
+                <div className="media-body mx-auto">
+                  <h1 className="text-white">Studique</h1>
+                  <h3 className="text-white mb-5">Crowdsourcing The College Experience</h3>
+                </div>
+              </div> */}
               <form className="form-inline d-flex">
                 <input type="text" className="form-control flex-fill mr-0 mr-sm-2 mb-3 mb-sm-0" id="inputText" placeholder="ASK A QUESTION"/>
-              <button type="submit" className="btn btn-primary mx-auto">Search</button>*/}
+                <button type="submit" className="btn btn-primary mx-auto">Search</button>
               </form>
-    
             </div>
           </div>
-        </div>
+        {/* </div>
+        </div> */}
       </section>
-    </body>
-  
-    <footer className="bg-primary text-center text-white">
-      <div className="container">
-        Copyright &copy; Your Website 2020
-      </div>
-    </footer>
     </div>
   );
 }
