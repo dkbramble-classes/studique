9dc3b12baf496bdfe91444a78f8d01e4ad58ba36
Kelly Hancox <hancoxk@mail.gvsu.edu>
2020-02-04 00:06:35 -0500

    whoops

--- src/frontpage.js
+++ src/frontpage.js
@@ -1,24 +1,31 @@
-import React from 'react';
-import './shared/css/style.css';
+import React from "react";
+import "./shared/css/style.css";
 import logo from "./shared/images/logo.png";
 
 function App() {
   return (
     <div className="App bg-image">
+       <link href="./style.css" rel="stylesheet"></link>
       <link href="./bootstrap.min.css" rel="stylesheet"></link>
-      <link href="./font-awesome.min.css" rel="stylesheet"></link>
-      <link href="https://fonts.googleapis.com/css?family=Varela+Round&display=swap" rel="stylesheet"></link>
+      {/* <link href="./font-awesome.min.css" rel="stylesheet"></link> */}
+      <link
+        href="https://fonts.googleapis.com/css?family=Varela+Round&display=swap"
+        rel="stylesheet"
+      ></link>
 
       <nav className="navbar navbar-expand-sm navbar-light" id="mainNavStdq">
         <div className="container">
           <a className="navbar-brand text-font" href="#page-top">
             <img className="brand-logo mr-2" src={logo} alt="LogoBrand" />
-            Studique</a>
+            Studique
+          </a>
           <div className="collapse navbar-collapse" id="navbarResponsive">
             <ul className="navbar-nav ml-auto">
               <li className="nav-item">
                 {/* <button type="button" class="btn btn-outline-info border border-info">Info</button> */}
-                <button type="submit" className=" btn nav-link text-font">Sign In</button>
+                <button type="submit" className=" btn nav-link text-font">
+                  Sign In
+                </button>
               </li>
               <li className="nav-item">
                 <button className="nav-link btn text-font">Sign up</button>
@@ -34,7 +41,9 @@ function App() {
         <div className="row">
           <div className="col-md-5 col-lg-8 mx-auto text-center">
             <h1 className="text-white mb-4">Studique</h1>
-            <h3 className="text-white mb-5">Crowdsourcing The College Experience</h3>
+            <h3 className="text-white mb-5">
+              Crowdsourcing The College Experience
+            </h3>
             {/* <div className="media">
 <img className="logo mr-3" src={logo} alt="LogoPage"/>
 <div className="media-body mx-auto">
@@ -43,8 +52,15 @@ function App() {
 </div>
 </div> */}
             <form className="form-inline d-flex">
-              <input type="text" className="form-control flex-fill mr-0 mr-sm-2 mb-3 mb-sm-0" id="inputText" placeholder="ASK A QUESTION" />
-              <button type="submit" className="btn btn-primary mx-auto">Search</button>
+              <input
+                type="text"
+                className="form-control flex-fill mr-0 mr-sm-2 mb-3 mb-sm-0"
+                id="inputText"
+                placeholder="ASK A QUESTION"
+              />
+              <button type="submit" className="btn btn-primary mx-auto">
+                Search
+              </button>
             </form>
           </div>
         </div>
@@ -56,7 +72,3 @@ function App() {
 }
 
 export default App;
-
-
-
-
