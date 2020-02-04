6d871b8c4826401991bddf4c782ee22c01a07760
Kelly Hancox <hancoxk@mail.gvsu.edu>
2020-02-03 21:36:29 -0500

    edited styles

--- src/frontpage.js
+++ src/frontpage.js
@@ -5,62 +5,58 @@ import logo from "./shared/images/logo.png";
 function App() {
   return (
     <div className="App bg-image">
-      <head>
-        <link href="./bootstrap.min.css" rel="stylesheet"></link>
-        <link href="./shared/css/style.css" rel="stylesheet"></link>
-        <link href="https://fonts.googleapis.com/css?family=Baloo+Bhai&display=swap" rel="stylesheet"></link>
-      </head>
-  
-      <nav className="navbar navbar-expand-sm navbar-light fixed-top" id="mainNav">
+      <link href="./bootstrap.min.css" rel="stylesheet"></link>
+      <link href="./font-awesome.min.css" rel="stylesheet"></link>
+      <link href="https://fonts.googleapis.com/css?family=Varela+Round&display=swap" rel="stylesheet"></link>
+
+      <nav className="navbar navbar-expand-sm navbar-light" id="mainNavStdq">
         <div className="container">
           <a className="navbar-brand text-font" href="#page-top">
-            <img className="brand-logo mr-2" src={logo} alt="LogoBrand" width="20px"/>
+            <img className="brand-logo mr-2" src={logo} alt="LogoBrand" />
             Studique</a>
           <div className="collapse navbar-collapse" id="navbarResponsive">
             <ul className="navbar-nav ml-auto">
-                <li className="nav-item">
-               {/* <button type="button" class="btn btn-outline-info border border-info">Info</button> */}
-               <button className="title-navbar btn text-font">Sign In</button>
-               </li>
-               <li className="nav-item">
-                 <button className="title-navbar btn text-font">Sign up</button>
-               </li>
-             </ul>
-           </div>
-         </div>
-       </nav>
-
-      {/* <div className="jumbotron mt-10"></div> */}
-      <section id="frontpage" className="frontpage-section">
-        {/* <div className="jumbotron jumbotron-fluid rounded mt-10">
-        <div className="container-fluid"> */}
-          <div className="row">
-            <div className="col-md-5 col-lg-8 mx-auto text-center">
-              <h1 className="text-white mb-4">Studique</h1>
-              <h3 className="text-white mb-5">Crowdsourcing The College Experience</h3>
-                {/* <div className="media">
-                <img className="logo mr-3" src={logo} alt="LogoPage"/>
-                <div className="media-body mx-auto">
-                  <h1 className="text-white">Studique</h1>
-                  <h3 className="text-white mb-5">Crowdsourcing The College Experience</h3>
-                </div>
-              </div> */}
-              <form className="form-inline d-flex">
-                <input type="text" className="form-control flex-fill mr-0 mr-sm-2 mb-3 mb-sm-0" id="inputText" placeholder="ASK A QUESTION"/>
-                <button type="submit" className="btn btn-primary mx-auto">Search</button>
-              </form>
-            </div>
+              <li className="nav-item">
+                {/* <button type="button" class="btn btn-outline-info border border-info">Info</button> */}
+                <button type="submit" className=" btn nav-link text-font">Sign In</button>
+              </li>
+              <li className="nav-item">
+                <button className="nav-link btn text-font">Sign up</button>
+              </li>
+            </ul>
           </div>
+        </div>
+      </nav>
+      {/* <div className="jumbotron mt-10"></div> */}
+      <section id="signup" className="signup-section my-5 imgbox center-fit">
+        {/* <div className="jumbotron jumbotron-fluid rounded mt-10">
+<div className="container-fluid"> */}
+        <div className="row">
+          <div className="col-md-5 col-lg-8 mx-auto text-center">
+            <h1 className="text-white mb-4">Studique</h1>
+            <h3 className="text-white mb-5">Crowdsourcing The College Experience</h3>
+            {/* <div className="media">
+<img className="logo mr-3" src={logo} alt="LogoPage"/>
+<div className="media-body mx-auto">
+<h1 className="text-white">Studique</h1>
+<h3 className="text-white mb-5">Crowdsourcing The College Experience</h3>
+</div>
+</div> */}
+            <form className="form-inline d-flex">
+              <input type="text" className="form-control flex-fill mr-0 mr-sm-2 mb-3 mb-sm-0" id="inputText" placeholder="ASK A QUESTION" />
+              <button type="submit" className="btn btn-primary mx-auto">Search</button>
+            </form>
+          </div>
+        </div>
         {/* </div>
-        </div> */}
-      </section> 
-
+</div> */}
+      </section>
     </div>
   );
 }
 
 export default App;
-  
-      
+
+
 
 
