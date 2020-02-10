9445b3e61e403dbba7d0d4705aba1257a6725df6
Jesse David <davidjes@mail.gvsu.edu>
2020-02-10 17:07:49 -0500

    beginning sprint 2

--- src/signUp/signUp.jsx
+++ src/signUp/signUp.jsx
@@ -1,65 +1,90 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
 import "./signUp.css";
 class SignUp extends Component {
-    render(){
-        return (
-        <div className = "popup">
-            <h2 className = "title">Sign-Up</h2>
-            <div>
-                <div>
-                <input
-                className="form-control flex-fill mr-0 mr-sm-2 mb-3 mb-sm-0"
-                style={{ width:"80%"}} 
-                id="inputText" type="text" 
-                value={this.state.displayName}
-                name = "displayName"
-                placeholder = "Full Name"
-                onChange={(e) => this.updateFormData(e)}></input></div>
-                <div>
-                <input 
-                className="form-control flex-fill mr-0 mr-sm-2 mb-3 mb-sm-0"
-                style={{ width:"80%"}} type="text" 
-                value={this.state.userEmail}
-                name = "userEmail"
-                placeholder = "Email"
-                onChange={(e) => this.addFormData(e)}></input></div>
-                <div>
-                <input className="form-control flex-fill mr-0 mr-sm-2 mb-3 mb-sm-0"
-                style={{ width:"80%"}} type="password" 
-                value = {this.state.userPassword}
-                name = "userPassword"
-                placeholder = "Password"
-                onChange={(e) => this.addFormData(e)}></input></div>
-                <div>
-                <h3>User Type:</h3>
-                <tbody>
-                    <td><input type="radio" name="faculty" />Faculty</td>
-                    <td><input type="radio" name="student"/>Student</td>
-                    </tbody>
-                </div>
-                    
-                <div>
-                        <button className="btn btn-primary mx-auto" onClick={() => this.doSignUp()}>Sign-Up</button>
-                </div>
-                <div>
-                    <button className="btn btn-secondary mx-auto" onClick={() => this.doGoogleSignUp()}>Sign-Up with Google</button>
-                </div>
-            </div>
+  render() {
+    return (
+      <div className="popup">
+        <h2 className="title">Sign-Up</h2>
+        <div>
+          <div>
+            <input
+              className="form-control flex-fill mr-0 mr-sm-2 mb-3 mb-sm-0"
+              style={{ width: "80%" }}
+              id="inputText"
+              type="text"
+              value={this.state.displayName}
+              name="displayName"
+              placeholder="Full Name"
+              onChange={e => this.updateFormData(e)}
+            ></input>
+          </div>
+          <div>
+            <input
+              className="form-control flex-fill mr-0 mr-sm-2 mb-3 mb-sm-0"
+              style={{ width: "80%" }}
+              type="text"
+              value={this.state.userEmail}
+              name="userEmail"
+              placeholder="Email"
+              onChange={e => this.addFormData(e)}
+            ></input>
+          </div>
+          <div>
+            <input
+              className="form-control flex-fill mr-0 mr-sm-2 mb-3 mb-sm-0"
+              style={{ width: "80%" }}
+              type="password"
+              value={this.state.userPassword}
+              name="userPassword"
+              placeholder="Password"
+              onChange={e => this.addFormData(e)}
+            ></input>
+          </div>
+          <div>
+            <h3>User Type:</h3>
+            <tbody>
+              <td>
+                <input type="radio" name="faculty" />
+                Faculty
+              </td>
+              <td>
+                <input type="radio" name="student" />
+                Student
+              </td>
+            </tbody>
+          </div>
+
+          <div>
+            <button
+              className="btn btn-primary mx-auto"
+              onClick={() => this.doSignUp()}
+            >
+              Sign-Up
+            </button>
+          </div>
+          <div>
+            <button
+              className="btn btn-secondary mx-auto"
+              onClick={() => this.doGoogleSignUp()}
+            >
+              Sign-Up with Google
+            </button>
+          </div>
         </div>
-        );
-    }
+      </div>
+    );
+  }
 
-    constructor(props){
-        super(props);
-        this.state = {
-            userEmail: "",
-            userPassword: ""
-        }
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      userEmail: "",
+      userPassword: ""
+    };
+  }
 
-    addFormData(ev){
-        this.setState({[ev.target.name]: ev.target.value});
-    }
-    
+  addFormData(ev) {
+    this.setState({ [ev.target.name]: ev.target.value });
+  }
 }
 export default SignUp;
