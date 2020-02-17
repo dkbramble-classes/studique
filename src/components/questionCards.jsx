fd749de25b7f7df9c197eaef1093605408e012e0
Kelly Hancox <hancoxk@mail.gvsu.edu>
2020-02-16 23:50:57 -0500

    editing now

--- src/components/questionCards.jsx
+++ src/components/questionCards.jsx
@@ -1,13 +1,36 @@
 import React from 'react';
 import '../css/questionCards.css';
+import { ReactComponent as UpArrow } from '../images/keyboard_arrow_up-24px.svg';
+import { ReactComponent as DownArrow } from '../images/keyboard_arrow_down-24px.svg';
+
 
 function questionCards() {
-  return (
-      <div>
-        <h1>question card page</h1>
-      </div>
-      
-  );
+
+
+
+
+return (
+<div className="qcard">
+<div className="qcardVotes">
+<div><UpArrow /></div>
+<div><h4>12</h4></div>
+<div><DownArrow /></div>
+</div>
+
+<div className="qcardRightContent">
+<div className="qcardProfile">
+<span>profile</span>
+</div>
+<div className="qcardTitle">
+<h5>How do I create a Hello World in React?</h5>
+</div>
+<div className="qcardTags">
+<div className="tags">CIS162</div>
+<div className="tags">computer science</div>
+</div>
+</div>
+</div>
+);
 }
 
 export default questionCards;
