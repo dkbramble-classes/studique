6bdc973268500d9cee1f365af8c6d926b313be99
Jesse David <davidjes@mail.gvsu.edu>
2020-02-21 17:33:36 -0500

    Getting that initial profile page stuff in

--- src/pages/profilePage.jsx
+++ src/pages/profilePage.jsx
@@ -1,12 +1,18 @@
 import React from 'react';
 import '../css/profilePage.css';
 
+
 function Profile() {
     return (
         <div>
-            <h1>profile page</h1>
             <h2 className="userDisplayName">User Name</h2>
             <p className="userEmail">Email: test@mail.gvsu.edu</p>
+            
+            <img
+              className="profileImage"
+              src={require("../images/profile.png")}
+              alt="profilePic"
+            />
             
         </div>
 
