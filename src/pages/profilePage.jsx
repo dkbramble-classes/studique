7d9ed7805a3e7f47d86dd1976b77b60e947374ea
Jesse David <davidjes@mail.gvsu.edu>
2020-02-20 20:27:43 -0500

    beginning profile page

--- src/pages/profilePage.jsx
+++ src/pages/profilePage.jsx
@@ -5,6 +5,9 @@ function Profile() {
     return (
         <div>
             <h1>profile page</h1>
+            <h2 className="userDisplayName">User Name</h2>
+            <p className="userEmail">Email: test@mail.gvsu.edu</p>
+            
         </div>
 
     );
