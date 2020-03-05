b93a7214e5192bbce3a74f2e1a901add9c2820cb
Kelly Hancox <hancoxk@mail.gvsu.edu>
2020-03-05 01:50:54 -0500

    used prettier

--- src/pages/questionFormPage.jsx
+++ src/pages/questionFormPage.jsx
@@ -4,8 +4,6 @@ import "../css/questionForm.css";
 class QuestionForm extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { title: "", body: "", tags: "" };
-    //this.updateClick = this.updateClick.bind(this);
   }
 
   render() {
@@ -16,55 +14,57 @@ class QuestionForm extends React.Component {
 
           <div className="qFormOuterForm">
             <div className="qFormInnerForm">
-          <div className="qFormSection">
-            <h4>Title</h4>
-            <div className="qFormSubtext">Be specific</div>
-            <div>
-              <form>
-                <input
-                  type="text"
-                  className="qFormInput"
-                  id="questionTitle"
-                  autoComplete="off"
-                  placeholder="e.g. Where can I find my tax forms as a student employee?"
-                />
-              </form>
-            </div>
-          </div>
+              <div className="qFormSection">
+                <h4>Title</h4>
+                <div className="qFormSubtext">Be specific</div>
+                <div>
+                  <form>
+                    <input
+                      type="text"
+                      className="qFormInput"
+                      id="questionTitle"
+                      autoComplete="off"
+                      placeholder="e.g. Where can I find my tax forms as a student employee?"
+                    />
+                  </form>
+                </div>
+              </div>
 
-          <div className="qFormSection">
-            <h4>Body</h4>
-            <div className="qFormSubtext">
-              Explain the context, why you need it, what you've tried, etc.
-            </div>
-            <div>
-              <form>
-                <textarea
-                  type="text"
-                  className="qFormInput"
-                  id="questionBody"
-                  autoComplete="off"
-                  rows="10"
-                  placeholder="e.g. I have already tried looking at this website page (include link to page) . . . "
-                />
-              </form>
-            </div>
-          </div>
+              <div className="qFormSection">
+                <h4>Body</h4>
+                <div className="qFormSubtext">
+                  Explain the context, why you need it, what you've tried, etc.
+                </div>
+                <div>
+                  <form>
+                    <textarea
+                      type="text"
+                      className="qFormInput"
+                      id="questionBody"
+                      autoComplete="off"
+                      rows="10"
+                      placeholder="e.g. I have already tried looking at this website page (include link to page) . . . "
+                    />
+                  </form>
+                </div>
+              </div>
 
-          <div className="qFormSection">
-            <h4>Tags</h4>
-            <div className="qFormSubtext">Separate multiple tags by commas</div>
-            <form>
-              <input
-                type="text"
-                className="qFormInput"
-                id="questionTags"
-                autoComplete="off"
-                placeholder="e.g. Student employee, Employment"
-              />
-            </form>
-          </div>
-          </div>
+              <div className="qFormSection">
+                <h4>Tags</h4>
+                <div className="qFormSubtext">
+                  Separate multiple tags by commas
+                </div>
+                <form>
+                  <input
+                    type="text"
+                    className="qFormInput"
+                    id="questionTags"
+                    autoComplete="off"
+                    placeholder="e.g. Student employee, Employment"
+                  />
+                </form>
+              </div>
+            </div>
           </div>
 
           <button type="submit" className="text-font qFormButton">
