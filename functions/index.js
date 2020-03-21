f54f3ee149f8b46e9a1bef9833657d824b2f061a
BrotherB1 <bassettl@mail.gvsu.edu>
2020-03-21 18:16:23 -0400

    Deletes algolia index on firebase index deletion.

--- functions/index.js
+++ functions/index.js
@@ -2,59 +2,35 @@
 const functions = require('firebase-functions');
 const algoliasearch = require('algoliasearch');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-exports.helloWorld = functions.https.onRequest((request, response) => {
- response.send("Hello from Firebase!");
-});
-
 // The Firebase Admin SDK to access the Firebase Realtime Database.
 const admin = require('firebase-admin');
 admin.initializeApp();
 
-// Take the text parameter passed to this HTTP endpoint and insert it into the
-// Realtime Database under the path /messages/:pushId/original
-exports.addMessage = functions.https.onRequest(async (req, res) => {
-    // Grab the text parameter.
-    const original = req.query.text;
-    // Push the new message into the Realtime Database using the Firebase Admin SDK.
-    const snapshot = await admin.database().ref('/messages').push({original: original});
-    // Redirect with 303 SEE OTHER to the URL of the pushed object in the Firebase console.
-    res.redirect(303, snapshot.ref.toString());
-});
-
-// Listens for new messages added to /messages/:pushId/original and creates an
-// uppercase version of the message to /messages/:pushId/uppercase
-exports.makeUppercase = functions.database.ref('/messages/{pushId}/original')
-    .onCreate((snapshot, context) => {
-        // Grab the current value of what was written to the Realtime Database.
-        const original = snapshot.val();
-        console.log('Uppercasing', context.params.pushId, original);
-        const uppercase = original.toUpperCase();
-        // You must return a Promise when performing asynchronous tasks inside a Functions such as
-        // writing to the Firebase Realtime Database.
-        // Setting an "uppercase" sibling in the Realtime Database returns a Promise.
-        return snapshot.ref.parent.child('uppercase').set(uppercase);
-    });
 const ALGOLIA_POSTS_INDEX_NAME = 'Questions';
 const client = algoliasearch(functions.config().algolia.app_id, functions.config().algolia.api_key);
 
 exports.updateAlgolia = functions.database.ref('/Questions/{q_id}/').onWrite(async (data, context) => {
         const index = client.initIndex(ALGOLIA_POSTS_INDEX_NAME);
 
-        const firebaseObject = {
-            Body: data.after.val().Body,
-            Title: data.after.val().Title,
-            Rating: data.after.val().Rating,
-            CreationDate: data.after.val().creationDate,
-            Tags: data.after.val().Tags,
-            //UserType: data.after.val().userType,
-            UserID: data.after.val().uid,
-            UserDisplayName: data.after.val().UserDisplayName,
-            Comments: data.after.val().Comments,
-            objectID: context.params.q_id
-        };
+        // An empty val() signifies deletion.
+        if(data.after.val() === null)
+        {
+            await index.deleteObject(context.params.q_id);
+        }
+        else {
+            const firebaseObject = {
+                Body: data.after.val().Body,
+                Title: data.after.val().Title,
+                Rating: data.after.val().Rating,
+                CreationDate: data.after.val().creationDate,
+                Tags: data.after.val().Tags,
+                //UserType: data.after.val().userType,
+                UserID: data.after.val().uid,
+                UserDisplayName: data.after.val().UserDisplayName,
+                Comments: data.after.val().Comments,
+                objectID: context.params.q_id
+            };
 
-        await index.saveObject(firebaseObject);
+            await index.saveObject(firebaseObject);
+        }
 });
