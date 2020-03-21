b846e770f07b2c79f45cc39ccd8fc3344cc5f73e
Jesse David <jdavid976@gmail.com>
2020-03-21 11:14:12 -0400

    Set photoURL when uploading profile pic

--- src/hooks/databaseHooks.js
+++ src/hooks/databaseHooks.js
@@ -1,193 +1,225 @@
 // Firebase App (the core Firebase SDK) is always required and must be listed first
-import {getUserInfo} from "./profileHooks";
+import { getUserInfo } from "./profileHooks";
 
 const firebase = require("firebase/app");
-const dotenv = require('dotenv');
+const dotenv = require("dotenv");
 require("firebase/database");
 require("firebase/auth");
 require("firebase/storage");
 
-
-
 dotenv.config();
 const firebaseConfig = {
-    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-    authDomain: "studique-3179a.firebaseapp.com",
-    databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
-    projectId: "studique",
-    storageBucket: "studique.appspot.com",
-    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGE_SENDER_ID,
-    appId: process.env.REACT_APP_FIREBASE_APP_ID
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: "studique-3179a.firebaseapp.com",
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+  projectId: "studique",
+  storageBucket: "studique.appspot.com",
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGE_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID
 };
 
 firebase.initializeApp(firebaseConfig);
 const database = firebase.database();
 const storage = firebase.storage();
 
-export async function initializeUser(user, permission, displayName)
-{
-    if(user != null) {
-        return await user.updateProfile({
-            displayName: displayName,
-        }).then( function() {
-            return firebase.database().ref('users/' + user.uid).set({
-                email: user.email,
-                permissions: permission
-            });
-        }).then(function() {
-            return getUserInfo().then(function (result) {
-                console.log(result);
-                return result["displayName"];
-            });
-        }).catch(function (error) {
-            console.log(error.message)
+export async function initializeUser(user, permission, displayName) {
+  if (user != null) {
+    return await user
+      .updateProfile({
+        displayName: displayName
+      })
+      .then(function() {
+        return firebase
+          .database()
+          .ref("users/" + user.uid)
+          .set({
+            email: user.email,
+            permissions: permission
+          });
+      })
+      .then(function() {
+        return getUserInfo().then(function(result) {
+          console.log(result);
+          return result["displayName"];
         });
-    }
-}
-
-export async function updateDisplayName(newName)
-{
-    let user = firebase.auth().currentUser;
-    return await user.updateProfile({
-        displayName: newName,
-    })
-}
-
-export function getUserMetadata(user)
-{
-    return database.ref('users/' + user.uid).once('value').then(function (snapshot) {
-        let info = snapshot.val();
-        return info.permissions;
-    }).catch(function(error) {
-        console.log(error.code);
+      })
+      .catch(function(error) {
         console.log(error.message);
+      });
+  }
+}
+
+export async function updateDisplayName(newName) {
+  let user = firebase.auth().currentUser;
+  return await user.updateProfile({
+    displayName: newName
+  });
+}
+
+export async function updatePhotoUrl(newURL) {
+  let user = firebase.auth().currentUser;
+  return await user.updateProfile({
+    photoUrl: newURL
+  });
+}
+
+export function getUserMetadata(user) {
+  return database
+    .ref("users/" + user.uid)
+    .once("value")
+    .then(function(snapshot) {
+      let info = snapshot.val();
+      return info.permissions;
+    })
+    .catch(function(error) {
+      console.log(error.code);
+      console.log(error.message);
     });
 }
 
-export function signOut(props){
-    firebase.auth().signOut().then(function() {
-        // Sign-out successful.
-        props.handleAuthed('');
-      }).catch(function(error) {
-        console.log(error.code);
-        console.log(error.message);
+export function signOut(props) {
+  firebase
+    .auth()
+    .signOut()
+    .then(function() {
+      // Sign-out successful.
+      props.handleAuthed("");
+    })
+    .catch(function(error) {
+      console.log(error.code);
+      console.log(error.message);
     });
 }
 
 export function createQuestion(title, body, tagList) {
-    const user = firebase.auth().currentUser;
+  const user = firebase.auth().currentUser;
 
-    const postData = {
-        uid: user.uid,
-        Body: body,
-        Title: title,
-        Rating: 0,
-        Tags: tagList,
-        creationDate: Math.round((new Date()).getTime() / 1000),
-    };
-    console.log(postData);
+  const postData = {
+    uid: user.uid,
+    Body: body,
+    Title: title,
+    Rating: 0,
+    Tags: tagList,
+    creationDate: Math.round(new Date().getTime() / 1000)
+  };
+  console.log(postData);
 
-    const newPostKey = firebase.database().ref().child('Questions/').push().key;
+  const newPostKey = firebase
+    .database()
+    .ref()
+    .child("Questions/")
+    .push().key;
 
-    return firebase.database().ref("Questions/" + newPostKey).set(postData);
+  return firebase
+    .database()
+    .ref("Questions/" + newPostKey)
+    .set(postData);
 }
 
 export function addComment(q_id, body) {
-    const user = firebase.auth().currentUser;
+  const user = firebase.auth().currentUser;
 
-    const postData = {
-        uid: user.uid,
-        Body: body,
-        creationDate: Math.round((new Date()).getTime() / 1000),
-    };
-    console.log(postData);
+  const postData = {
+    uid: user.uid,
+    Body: body,
+    creationDate: Math.round(new Date().getTime() / 1000)
+  };
+  console.log(postData);
 
-    const newPostKey = firebase.database().ref().child('Questions/' + q_id + '/Comments/').push().key;
+  const newPostKey = firebase
+    .database()
+    .ref()
+    .child("Questions/" + q_id + "/Comments/")
+    .push().key;
 
-    return firebase.database().ref("Questions/" + q_id + '/Comments/' + newPostKey).set(postData);
+  return firebase
+    .database()
+    .ref("Questions/" + q_id + "/Comments/" + newPostKey)
+    .set(postData);
 }
 
-export async function updateRating( q_id, voteDir)
-{
-    const user = firebase.auth().currentUser;
-    return firebase.database().ref("Questions/" + q_id + '/').once('value').then(function(snapshot) {
-        let rating = snapshot.val().Rating;
-        let current_upvotes_list = snapshot.val().UpVotes;
-        let current_downvotes_list = snapshot.val().DownVotes;
-        if(voteDir === "UpVotes")
-        {
+export async function updateRating(q_id, voteDir) {
+  const user = firebase.auth().currentUser;
+  return firebase
+    .database()
+    .ref("Questions/" + q_id + "/")
+    .once("value")
+    .then(function(snapshot) {
+      let rating = snapshot.val().Rating;
+      let current_upvotes_list = snapshot.val().UpVotes;
+      let current_downvotes_list = snapshot.val().DownVotes;
+      if (voteDir === "UpVotes") {
+        rating += 1;
+        if (current_upvotes_list === undefined) {
+          current_upvotes_list = [user.uid];
+        } else {
+          current_upvotes_list.push(user.uid);
+        }
+        if (current_downvotes_list !== undefined) {
+          let pos = current_downvotes_list.indexOf(user.uid);
+          if (pos !== -1) {
             rating += 1;
-            if(current_upvotes_list === undefined)
-            {
-                current_upvotes_list = [user.uid];
-            }else{
-                current_upvotes_list.push(user.uid)
-            }
-            if(current_downvotes_list !== undefined) {
-                let pos = current_downvotes_list.indexOf(user.uid);
-                if (pos !== -1) {
-                    rating += 1;
-                    current_downvotes_list.splice(pos, 1);
-                }
-            }
-            else
-            {
-                current_downvotes_list = []
-            }
+            current_downvotes_list.splice(pos, 1);
+          }
+        } else {
+          current_downvotes_list = [];
         }
-        else
-        {
-            if(current_downvotes_list === undefined)
-            {
-                current_downvotes_list = [user.uid];
-            }
-            else{
-                console.log(current_downvotes_list);
-                current_downvotes_list.push(user.uid)
-            }
+      } else {
+        if (current_downvotes_list === undefined) {
+          current_downvotes_list = [user.uid];
+        } else {
+          console.log(current_downvotes_list);
+          current_downvotes_list.push(user.uid);
+        }
+        rating -= 1;
+        if (current_upvotes_list !== undefined) {
+          let pos = current_upvotes_list.indexOf(user.uid);
+          if (pos !== -1) {
             rating -= 1;
-            if(current_upvotes_list !== undefined) {
-                let pos = current_upvotes_list.indexOf(user.uid);
-                if (pos !== -1) {
-                    rating -= 1;
-                    current_upvotes_list.splice(pos, 1);
-                }
-            }
-            else
-            {
-                current_upvotes_list = []
-            }
+            current_upvotes_list.splice(pos, 1);
+          }
+        } else {
+          current_upvotes_list = [];
         }
-        firebase.database().ref("Questions/" + q_id + '/').update({
-            Rating: rating,
-            UpVotes: current_upvotes_list,
-            DownVotes: current_downvotes_list,
-        }).catch(function (error) {
-            console.log(error.code);
-            console.log(error.message);
+      }
+      firebase
+        .database()
+        .ref("Questions/" + q_id + "/")
+        .update({
+          Rating: rating,
+          UpVotes: current_upvotes_list,
+          DownVotes: current_downvotes_list
+        })
+        .catch(function(error) {
+          console.log(error.code);
+          console.log(error.message);
         });
-        return rating;
+      return rating;
     });
 }
 
-export function getRatingInfo(q_id)
-{
-    return firebase.database().ref("Questions/" + q_id + '/').once('value').then(function(snapshot) {
-        let rating = snapshot.val().Rating;
-        let color = "Neutral";
-        const user = firebase.auth().currentUser;
-        if(snapshot.val().UpVotes !== undefined && snapshot.val().UpVotes.includes(user.uid))
-        {
-            color = "Up";
-        }
-        else if(snapshot.val().DownVotes !== undefined && snapshot.val().DownVotes.includes(user.uid))
-        {
-            color = "Down";
-        }
-        return {Rating: rating, color: color};
-    })
+export function getRatingInfo(q_id) {
+  return firebase
+    .database()
+    .ref("Questions/" + q_id + "/")
+    .once("value")
+    .then(function(snapshot) {
+      let rating = snapshot.val().Rating;
+      let color = "Neutral";
+      const user = firebase.auth().currentUser;
+      if (
+        snapshot.val().UpVotes !== undefined &&
+        snapshot.val().UpVotes.includes(user.uid)
+      ) {
+        color = "Up";
+      } else if (
+        snapshot.val().DownVotes !== undefined &&
+        snapshot.val().DownVotes.includes(user.uid)
+      ) {
+        color = "Down";
+      }
+      return { Rating: rating, color: color };
+    });
 }
 
-export {
-    storage, firebase as default
-}
+export { storage, firebase as default };
