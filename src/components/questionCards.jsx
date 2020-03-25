23dc511921d1c38a3530098ad331f908ecbabfd2
BrotherB1 <bassettl@mail.gvsu.edu>
2020-03-25 13:39:23 -0400

    Added state for questionPhoto

--- src/components/questionCards.jsx
+++ src/components/questionCards.jsx
@@ -2,7 +2,7 @@ import React, {useState} from "react";
 import "../css/questionCards.css";
 import { ReactComponent as UpArrow } from "../images/keyboard_arrow_up-24px.svg";
 import { ReactComponent as DownArrow } from "../images/keyboard_arrow_down-24px.svg";
-import {storage, addComment, updateRating, getRatingInfo, getRating} from "../hooks/databaseHooks";
+import {storage, addComment, updateRating, getRatingInfo, getRating, getPhotoURL} from "../hooks/databaseHooks";
 import 'firebase/storage';
 
 function QuestionCards(props) {
@@ -24,7 +24,7 @@ function QuestionCards(props) {
     ))
   }
 
-  if(props.userPhoto != null && props.userPhoto !== undefined){
+  if(props.userPhoto !== null && props.userPhoto !== undefined){
     url = props.userPhoto;
   }
 
@@ -36,6 +36,7 @@ function QuestionCards(props) {
   };
   const [voteColor, updateColor] = useState(colors["Neutral"]);
   const [bodyInput, setBodyInput] = useState("");
+  const [questionPhoto, updateQuestionPhoto] = useState("");
 
   const q_id = props.objectId;
   console.log('props', props);
@@ -90,6 +91,15 @@ function QuestionCards(props) {
         })
       });
     }
+  }
+
+  function getQuestionPhoto(uid){
+    getPhotoURL(uid).then( function (url) {
+      updateQuestionPhoto(url);
+    }).catch(function(error) {
+      console.log(error.code);
+      console.log(error.message);
+    });
   }
 
   function handleBodyInput(ev) {
@@ -219,8 +229,9 @@ function QuestionCards(props) {
           <div className="qcardProfile">
             <img 
               className="qcardProfileLogo"
-              src="https://firebasestorage.googleapis.com/v0/b/studique.appspot.com/o/images%2Fhancoxk_200x200?alt=media&token=e577b6e9-7a9b-4711-ad8b-1556f1068bf6"
+              src={questionPhoto}
               alt="profilePic"
+              onLoad={updateQuestionPhoto(props.UserID)}
             />
             <span>{props.userDisplayName}</span>
           </div>
