5f74676601cf974a8c343fcc027edc9b07118cda
Kelly Hancox <hancoxk@mail.gvsu.edu>
2020-03-30 22:22:33 -0400

    not working yet

--- src/components/profileQuestions.jsx
+++ src/components/profileQuestions.jsx
@@ -1,50 +1,78 @@
 import React, { useState } from "react";
 import "../css/profilePage.css";
-import { getQuestionsByUser, getUid} from "../hooks/databaseHooks";
+import { getQuestionsByUser, getUid } from "../hooks/databaseHooks";
 import QuestionCards from "./questionCards";
 
-function ProfileQuestions(props) {
+function ProfileQuestions() {
   //gets the functions from storage refences the image storage in firebase by the children
   //gets the download url then sets the image from firebase as the value for the imgUrl key:
-    let currentQuestions = "";
-    let uid = getUid();
+  const [questions, loading] = useAsyncHook();
+  console.log("questions", questions);
+  console.log(typeof(questions));
 
-    getQuestionsByUser(uid)
-      .then(function(snapshots) {
-        console.log('snapshots', snapshots)
-        currentQuestions = snapshots;
-        //0: {key: ___, body: ___}
-        Object.entries(snapshots).forEach(item => { return <div>item</div> }); //console.log("item", item)});
-
-      })
-      .catch(function(error) {
-        return (<div className="profile">There was an error. Please try again.</div>);
-      });
-
-      
-  
-      return <div className="profile">
-      {Object.values(currentQuestions).map(item => {
-        return (
-          <QuestionCards
-            key={item.key}
-            objectId={item.objectID}
-            title={item.Title}
-            body={item.Body}
-            rating={item.Rating}
-            creationDate={item.creationDate}
-            tags={item.Tags}
-            userId={item.uid}
-            userDisplayName={item.UserDisplayName}
-            userPhoto={item.UserPhoto}
-          />
-        //what it doesn't have: objectID, UserPhoto
-        //has extra: UpVotes
-        //need to iterate over the map differently
-
-        );
-      })}
+  return (
+    <div>
+      {loading === "false" ? (
+        <h3>Loading...</h3>
+      ) : questions === undefined ? (
+        <h3>No results found</h3>
+      ) : (
+        <div>
+        <h3>Questions that you asked:</h3>
+          { Object.entries(questions).forEach(item => { return <div>item</div> }) }
       </div>
+      )}
+    </div>
+  );
+}
+
+function useAsyncHook() {
+  const [questions, setQuestions] = React.useState();
+  const [loading, setLoading] = React.useState("false");
+
+  let uid = getUid();
+
+  React.useEffect(() => {
+    async function fetchSearch() {
+      try {
+        setLoading("true");
+        const results = await getQuestionsByUser(uid)
+          .then(function(snapshots) {
+            //console.log("snapshots", snapshots);
+            //0: {key: ___, body: ___}
+            // Object.entries(snapshots).forEach(item => {
+            //   return <div>item</div>
+            // }
+            //setQuestions(snapshots); //console.log("item", item)});
+            setLoading("false");
+
+            return snapshots;
+          })
+          .catch(function(error) {
+            return (
+              <div className="profile">
+                There was an error. Please try again.
+              </div>
+            );
+          });
+          console.log('results', results);
+          const finalResults = await results;
+          setQuestions(finalResults);
+          
+      } catch (error) {
+        setQuestions(undefined);
+        setLoading("null");
+      }
+      
+    }
+    
+    fetchSearch();
+    
+  });
+
+  //console.log('questions', questions);
+
+  return [questions, loading];
 }
 
 export default ProfileQuestions;
