1b00d523fc938fe7b8be51fae927a0705435e6c9
Dane <dkbrambled@gmail.com>
2020-04-01 21:44:45 -0400

    Removed sort if click on share knowledge button

--- src/components/questionList.jsx
+++ src/components/questionList.jsx
@@ -16,14 +16,25 @@ function QuestionList(props) {
     setSort(selectedOption.value);
   };
 
+  function DropDown() {
+    const sort = window.location.href.split('/sort/')[1] === null ? null : window.location.href.split('/sort/')[1];
+    if (sort){
+      return(<div/>);
+    } 
+  
+    return (        
+      <form className="form-inline pt-2 rounded-0 text-font">
+        <p className="text-font text-white mt-3">Sort By:</p>
+        <Select options={options} className="text-font dropdown text-left align-left" onChange={handleDropDown}  defaultValue={ { value: 'rating', label: 'Rating' }}/>
+    </form>
+    );
+  }
+
   return (
       <div className="qlistPage">
 
       <div className="qlistList">
-        <form className="form-inline pt-2 rounded-0 text-font">
-          <p className="text-font text-white mt-3">Sort By:</p>
-          <Select options={options} className="text-font dropdown text-left align-left" onChange={handleDropDown}  defaultValue={ { value: 'rating', label: 'Rating' }}/>
-        </form>
+        <DropDown/>
         <AlgoliaSearch sortOption={sortOption}/>
 
         <br/>
